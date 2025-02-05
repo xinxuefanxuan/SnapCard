878cafded077d52b24adf8c87f58bd2625366e25
xinxuefanxuan <2809832579@qq.com>
2025-02-05 22:38:58 +0800

    初始构建

---
+++ back/SnapCard/src/main/java/yyh/snapcard/SnapCardApplication.java
@@ -0,0 +1,13 @@
+package yyh.snapcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapCardApplication.class, args);
+    }
+
+}
---
+++ back/SnapCard/src/test/java/yyh/snapcard/SnapCardApplicationTests.java
@@ -0,0 +1,13 @@
+package yyh.snapcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
